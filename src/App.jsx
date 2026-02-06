b9c03cd6ecd1dcbd177b6316ec9890ca21ff8fad
sohel khan <sohelkhan060404@gmail.com>
2026-02-07 02:39:06 +0530

    app is wrap with auth and task comtext

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage";
+
 
 const App = () => {
+  useEffect(() => {
+    // setLocalStorage();
+    getLocalStorage()
+  });
+
   return (
     <>
-      {/* <Login /> */}
+      <Login />
       {/* <EmployeeDashboard/> */}
-      <AdminDashboard />
+      {/* <AdminDashboard /> */}
     </>
   );
 };
