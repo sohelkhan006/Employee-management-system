960662fe2c7b779d1e5a3945ffcc13356c41bf16
sohel khan <sohelkhan060404@gmail.com>
2026-02-09 21:19:30 +0530

    user login auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage";
-
+import { authContext } from "./context/AuthProvider";
 
 const App = () => {
-  useEffect(() => {
-    setLocalStorage();
-    getLocalStorage()
-  });
+  const [user, setUser] = useState(null);
+  // useEffect(() => {
+  //   setLocalStorage();
+  //   getLocalStorage()
+  // });
+
+  const handleUser = (email, password) => {
+    if (email === "admin@me.com" && password === "123") {
+      setUser("admin");
+      console.log(user);
+    } else if (email === "user@me.com" && password === "123") {
+      setUser("employee");
+      console.log(user);
+    } else {
+      alert("Invalid credential");
+    }
+  };
+
+  const data = useContext(authContext);
+  console.log(data)
 
   return (
     <>
-      <Login />
+      {!user && <Login handleUser={handleUser} />}
+      {user === "admin" && <AdminDashboard />}
+      {user === "employee" && <EmployeeDashboard />}
       {/* <EmployeeDashboard/> */}
       {/* <AdminDashboard /> */}
     </>
