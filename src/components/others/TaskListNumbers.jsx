aa67657cd2357972de9a578c543e937c281bfdf5
sohel khan <sohelkhan060404@gmail.com>
2026-02-09 21:14:04 +0530

    update values

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -4,20 +4,20 @@ const TaskListNumbers = () => {
   return (
     <div className="flex justify-between gap-5 w-full mt-10">
       <div className="bg-red-400 w-[45%] px-10 py-6 rounded-xl hover:bg-red-600">
-        <h2 className="text-3xl font-semibold">0</h2>
+        <h2 className="text-3xl font-semibold">1</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
       <div className="bg-blue-400 w-[45%] px-10 py-6 rounded-xl hover:bg-blue-600">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl font-semibold">3</h2>
+        <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
       <div className="bg-green-400 w-[45%] px-10 py-6 rounded-xl hover:bg-green-500">
         <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h3 className="text-xl font-medium">Accepted Task</h3>
       </div>
       <div className="bg-yellow-400 w-[45%] px-10 py-6 rounded-xl hover:bg-yellow-500">
         <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
     </div>
   );
