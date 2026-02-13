47ce4f31fc5bed6ec9101e177f1f0fd44e7713ae
sohel khan <sohelkhan060404@gmail.com>
2026-02-13 19:24:06 +0530

    auth data is added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,23 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from "react";
+import { getLocalStorage, setLocalStorage } from "../utils/LocalStorage";
 
-export const authContext = createContext()
-const AuthProvider = ({children}) => {
+export const authContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  
+  const [userdata, setUserData] = useState(null);
+
+  useEffect(() => {
+    setLocalStorage();
+    const { employees, admin } = getLocalStorage();
+    setUserData({ employees, admin });
+  }, []);
+
   return (
     <div>
-      <authContext.Provider value={'sohel'}>
-        {children}
-      </authContext.Provider>
+      <authContext.Provider value={userdata}>{children}</authContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
