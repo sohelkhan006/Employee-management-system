fdc7ddbd7cabbdd4710cd7510e246a6a5a5bae52
sohel khan <sohelkhan060404@gmail.com>
2026-02-13 19:15:02 +0530

    pass props to employee dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from '../others/Header'
-import TaskListNumbers from '../others/TaskListNumbers'
-import TaskList from '../TaskList/TaskList'
+import React from "react";
+import Header from "../others/Header";
+import TaskListNumbers from "../others/TaskListNumbers";
+import TaskList from "../TaskList/TaskList";
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({ data }) => {
   return (
-    <div className='p-10 bg-[#1c1c1c]  min-h-screen w-full'>
-      <Header/>
-      <TaskListNumbers />
-      <TaskList />
+    <div className="p-10 bg-[#1c1c1c]  min-h-screen w-full">
+      <Header data={data} />
+      <TaskListNumbers data={data} />
+      <TaskList data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
