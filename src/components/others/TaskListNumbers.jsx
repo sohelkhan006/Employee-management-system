86de190c0d16a585784b9b2bad0f8e786931f5ce
sohel khan <sohelkhan060404@gmail.com>
2026-02-13 19:18:19 +0530

    pass props to tasklistnumber

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-const TaskListNumbers = () => {
+const TaskListNumbers = ({ data }) => {
+  if (!data) return null;
   return (
     <div className="flex justify-between gap-5 w-full mt-10">
       <div className="bg-red-400 w-[45%] px-10 py-6 rounded-xl hover:bg-red-600">
-        <h2 className="text-3xl font-semibold">1</h2>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
       <div className="bg-blue-400 w-[45%] px-10 py-6 rounded-xl hover:bg-blue-600">
-        <h2 className="text-3xl font-semibold">3</h2>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.completed}</h2>
         <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
       <div className="bg-green-400 w-[45%] px-10 py-6 rounded-xl hover:bg-green-500">
-        <h2 className="text-3xl font-semibold">0</h2>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.active}</h2>
         <h3 className="text-xl font-medium">Accepted Task</h3>
       </div>
       <div className="bg-yellow-400 w-[45%] px-10 py-6 rounded-xl hover:bg-yellow-500">
-        <h2 className="text-3xl font-semibold">0</h2>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.failed}</h2>
         <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
     </div>
