9220f3a937a45c07743cf02bdd317d02accff6f1
sohel khan <sohelkhan060404@gmail.com>
2026-02-13 19:22:35 +0530

    render task in tasklist

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,81 +1,42 @@
 import React from "react";
+import AcceptTask from "./AcceptTask";
+import NewTask from "./NewTask";
+import CompleteTask from "./CompleteTask";
+import Failedtask from "./Failedtask";
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
   return (
     <div
       id="tasklist"
       className="flex items-center justify-start py-3 h-[55vh] w-full gap-4 mt-10 overflow-x-auto"
     >
-      <div className="shrink-0 h-full w-[300px] p-4 bg-yellow-300 rounded-xl">
-        <div className="flex items-center justify-between">
-          <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-            High
-          </h2>
-          <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
-        </div>
-        <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-          doloribus exercitationem possimus velit voluptatem.
-        </p>
-      </div>
+      {data.tasks.map((e,idx) => {
+        if (e.newTask) {
+          return <NewTask key={idx}/>;
+        }
+        if (e.active) {
+          return <AcceptTask key={idx}/>;
+        }
+        if (e.completed) {
+          return <CompleteTask key={idx}/>;
+        }
+        if (e.failed) {
+          return <Failedtask key={idx}/>;
+        }
 
-       <div className="shrink-0 h-full w-[300px] p-4 bg-blue-300 rounded-xl">
-        <div className="flex items-center justify-between">
-          <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-            High
-          </h2>
-          <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
-        </div>
-        <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-          doloribus exercitationem possimus velit voluptatem.
-        </p>
-      </div>
-
-       <div className="shrink-0 h-full w-[300px] p-4 bg-green-300 rounded-xl">
-        <div className="flex items-center justify-between">
-          <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-            High
-          </h2>
-          <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
-        </div>
-        <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-          doloribus exercitationem possimus velit voluptatem.
-        </p>
-      </div>
-
-       <div className="shrink-0 h-full w-[300px] p-4 bg-cyan-300 rounded-xl">
-        <div className="flex items-center justify-between">
-          <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-            High
-          </h2>
-          <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
-        </div>
-        <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-          doloribus exercitationem possimus velit voluptatem.
-        </p>
-      </div>
-
-       <div className="shrink-0 h-full w-[300px] p-4 bg-pink-400 rounded-xl">
-        <div className="flex items-center justify-between">
-          <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-            High
-          </h2>
-          <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
-        </div>
-        <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-          doloribus exercitationem possimus velit voluptatem.
-        </p>
-      </div>
-
+        // switch (true) {
+        //   case e.newTask:
+        //     return <NewTask key={idx}/>;
+        //   case e.active:
+        //     return <AcceptTask key={idx}/>;
+        //   case e.completed:
+        //     return <CompleteTask key={idx}/>;
+        //   case e.failed:
+        //     return <Failedtask key={idx}/>;
+        //   default:
+        //     return null;
+        // }
+      })}
     </div>
   );
 };
