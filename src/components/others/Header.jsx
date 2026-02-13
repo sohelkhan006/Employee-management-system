481ae87b307bad6adc6feceeefd4ef62a29bc7c2
sohel khan <sohelkhan060404@gmail.com>
2026-02-13 19:16:46 +0530

    pass rpops to header

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { setLocalStorage } from "../../utils/LocalStorage";
 
-const Header = () => {
+const Header = ({ data }) => {
+  // const [username, setUserName] = useState('')
+  // if (!data) {
+  //   setUserName('admin')
+  // }else{
+  //   setUserName(data.firstName)
+  // }
+
+  const logOutUser = () => {
+    localStorage.removeItem("loggedInUser", "");
+    window.location.reload();
+  };
+
   return (
     <div className="flex justify-between items-end">
       <h1 className="text-2xl font-medium  text-white">
         hello <br />
-        <span className="text-3xl font-semibold text-white">Sohel 👋</span>
+        <span className="text-3xl font-semibold text-white">{data?.firstName || "Admin"}👋</span>
       </h1>
       <button
         className="bg-red-600 text-white px-6 py-2 rounded-sm text-xl font-medium"
+        onClick={logOutUser}
       >
         Log out
       </button>
