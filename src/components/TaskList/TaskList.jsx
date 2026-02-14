bcd67ce0a26499b4ba317885e039e6f451649dba
sohel khan <sohelkhan060404@gmail.com>
2026-02-14 20:42:40 +0530

    props data is set

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,6 +5,7 @@ import CompleteTask from "./CompleteTask";
 import Failedtask from "./Failedtask";
 
 const TaskList = ({ data }) => {
+  console.log(data)
   return (
     <div
       id="tasklist"
@@ -12,16 +13,16 @@ const TaskList = ({ data }) => {
     >
       {data.tasks.map((e,idx) => {
         if (e.newTask) {
-          return <NewTask key={idx}/>;
+          return <NewTask key={idx} data={e}/>;
         }
         if (e.active) {
-          return <AcceptTask key={idx}/>;
+          return <AcceptTask key={idx} data={e}/>;
         }
         if (e.completed) {
-          return <CompleteTask key={idx}/>;
+          return <CompleteTask key={idx} data={e}/>;
         }
         if (e.failed) {
-          return <Failedtask key={idx}/>;
+          return <Failedtask key={idx} data={e}/>;
         }
 
         // switch (true) {
