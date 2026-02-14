65990df3bc2646199edcbba985af20083decc5b7
sohel khan <sohelkhan060404@gmail.com>
2026-02-14 20:41:57 +0530

    props data is set

--- src/components/TaskList/Failedtask.jsx
+++ src/components/TaskList/Failedtask.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-const Failedtask = () => {
+const Failedtask = ({ data }) => {
   return (
     <div className="shrink-0 h-full w-[350px] p-4 bg-cyan-300 rounded-xl">
       <div className="flex items-center justify-between">
         <h2 className="bg-red-400 px-4 py-2 text-sm font-semibold rounded-xl">
-          High
+          {data.category}
         </h2>
-        <h3 className="px-4 py-2 text-sm font-semibold">03 feb 2026</h3>
+        <h3 className="px-4 py-2 text-sm font-semibold">{data.date}</h3>
       </div>
-      <h2 className="mt-5 font-semibold text-2xl">Make a project</h2>
-      <p className="text-sm mt-2">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam
-        doloribus exercitationem possimus velit voluptatem.
-      </p>
+      <h2 className="mt-5 font-semibold text-2xl">{data.title}</h2>
+      <p className="text-sm mt-2">{data.description}</p>
       <div className="mt-4">
         <button className="w-full">Failed</button>
       </div>
